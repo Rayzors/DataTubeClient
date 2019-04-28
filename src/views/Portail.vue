1b7da4ae03fb391c75bb363f380f45b85b792c85
Punkte <paartheepan94@gmail.com>
2019-04-28 20:14:04 +0200

    feat: hold btn with svg

--- src/views/Portail.vue
+++ src/views/Portail.vue
@@ -9,11 +9,16 @@
     <div class="portail__image" v-parallax="0.2">
       <img :src="image" alt="image avec des gens qui font de la dataviz">
     </div>
-    <router-link to="/home">
-      <div class="portail__btn">
-        Maintenez
-      </div>
-    </router-link>
+    <svg class="portail__link" width="424px" height="393px" viewBox="0 0 424 393" >
+      <g class="portail__btn" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" x="10" y="20">
+        <router-link to="/home">
+          <g id="Homepage" transform="translate(-382, -378)" fill="#EE5355">
+            <path d="M601.59238,772.334192 C651.214169,772.334192 683.480778,730.104598 729.813504,694.506303 C782.005097,654.40653 807.385604,645.424244 807.385604,581.269504 C807.385604,540.369726 778.679062,521.017911 753.068092,479.238968 C702.842834,397.306963 661.429396,380.661357 591.546088,380.661357 C542.432601,380.661357 541.28582,430.936492 503.628281,478.373737 C460.38011,532.853507 384.385604,516.531366 384.385604,581.269504 C384.385604,648.62958 483.866693,673.872139 517.856338,694.506303 C544.977173,710.970603 554.763327,772.334192 601.59238,772.334192 Z" id="Oval" transform="translate(595.885604, 576.497775) rotate(1.000000) translate(-595.885604, -576.497775) "/>
+          </g>
+          <text class="portail__hold" x="10" y="20" font-size="26px" fill="#fff">Maintenez</text>
+        </router-link>
+      </g>  
+    </svg>
   </div>
 </template>
 
@@ -34,9 +39,9 @@ export default {
 
 <style lang="scss" scoped>
 @import '../assets/fonts/geomanist.css';
+@import url('https://fonts.googleapis.com/css?family=Josefin+Sans:600');
 $main-red: #ee5355;
 .portail {
-
   * {
     box-sizing: border-box;
   }
@@ -60,6 +65,21 @@ $main-red: #ee5355;
     }
   }
 
+  &__link {
+    position: absolute;
+    bottom: -18%; // a animer
+    right: -1%; // a animer
+    // passer la hauteur et la largeur a 100% au maintiens
+  }
+
+  &__hold {
+    position: absolute;
+    
+    font-size: 26px;
+    transform: rotate(6deg) translate(39%, 31%);
+    font-weight: bold;
+  }
+
   &__btn {
     position: absolute;
     bottom: 0;
@@ -68,19 +88,27 @@ $main-red: #ee5355;
     justify-content: center;
     align-items: center;
     width: 30%;
-    padding: 30px;
     border: solid 2px $main-red;
 
-    background-color: $main-red;
+    fill: $main-red;
 
     color: #FFF;
-
-    transition: background-color 0.3s, border-width 0.3s;
+    transition: fill 3s;
 
     &:hover {
-      background-color: #FFF;
-      color: $main-red;
+      g {
+        fill: #FFF;
+        stroke: $main-red;
+        stroke-width: 6px;
+      }
+      .portail__hold {
+        fill: $main-red;
+      }
     }
+
+    text {
+      font-family: 'Josefin Sans', sans-serif;
+    } 
   }
 }
 
