ec355eadc3a67ce64a4d4c30246d38e286575a52
Punkte <paartheepan94@gmail.com>
2019-05-23 20:02:28 +0200

    wip: d3 barchart

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -4,14 +4,16 @@
 
 <script>
 import * as d3 from 'd3';
-import chartData from '../assets/test.json'
 export default {
   data() {
     return {
-      chartData: {},
+      chartDatas: [],
     };
   },
   mounted () {
+    const days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+    days.forEach(category => this.chartDatas.push({category}));
+
     const margin = {
       top: 20,
       right: 20,
@@ -20,9 +22,6 @@ export default {
     };
     const width = 600 - margin.left - margin.right;
     const height = 300 - margin.top - margin.bottom;
-
-    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
-    const y = d3.scaleLinear().rangeRound([height, 0]);
 
 
 
@@ -33,29 +32,70 @@ export default {
                   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     d3.json('https://datatubeapi.kevinmanssat.fr/ressources/country/FR/category/10/subscribers/10000-100000')
-      .then(function(data) {
+      .then((data) => {
         const { numberOfPublicationByDay } = data
         numberOfPublicationByDay.push(numberOfPublicationByDay.shift())
-        x.domain(numberOfPublicationByDay.map((d) => d.label))
-        y.domain([0, d3.max(numberOfPublicationByDay, (d) => d.value)])
+
+        this.chartDatas = [
+          ...this.chartDatas,
+        ];
+
+        const categoriesNames = this.chartDatas.values.map((d) => d.label);
+        const rateNames = this.chartDatas.values.map((d) => d.value);
+
+        const x0 = d3.scaleBand()
+                .rangeRound([0, width]).padding(0.1)
+                .domain(categoriesNames);
+        const x1 = d3.scaleBand()
+                .rangeRound([0, width]).padding(0.1)
+                .domain(rateNames)
+          .rangeRoundBands([0, x0.bandwidth()]);
+        
+        const y = d3.scaleLinear().rangeRound([height, 0])
+                    .domain([0, d3.max(data, function(categorie) { return d3.max(categorie.values, function(d) { return d.value; }); })]);
+
+
+        const xAxis = d3.axisBottom(x0)
+                        .tickFormat(0)
+
+        const yAxis = d3.axisLeft(y)
+
+        x0.domain(this.chartDatas.values.map((d, i) => d.label))
+        y.domain([0, d3.max(this.chartDatas.values, (d) => d.value)])
 
         g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
+          .attr("class", "x axis")
+          .attr("transform", `translate(0,${height})`)
+          .call(xAxis);
             
         g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y))
+          .attr("class", "y axis")
+          .call(yAxis)
 
-        g.selectAll(".bar")
-            .data(numberOfPublicationByDay)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", (d) => x(d.label))
-            .attr("width", x.bandwidth())
-            .attr("y", (d) => y(d.value) )
-            .attr("height", (d) =>  height - y(d.value))
+        const slice = g.selectAll('.slice')
+                        .data(this.chartDatas)
+                        .enter()
+                          .append("g")
+                            .attr("class", "g")
+                            .attr("transform",(d, i) => `translate(${x0(d.value)},0)`);
+        console.log(this.chartDatas.values)
+        slice.selectAll("rect")
+              .data((d) => d.values)
+              .enter()
+                .append("rect")
+                  .attr("width", x0.bandwidth())
+                  .attr("x", (d) => x0(d.label))
+                  .attr("y", (d) => y(d.value))
+                  .attr("height", (d) => height - y(d.value))
+
+        // g.selectAll(".bar")
+        //     .data(numberOfPublicationByDay)
+        //     .enter().append("rect")
+        //     .attr("class", "bar")
+        //     .attr("x", (d) => x1(d.label))
+        //     .attr("width", x1.bandwidth())
+        //     .attr("y", (d) => y(d.value) )
+        //     .attr("height", (d) =>  height - y(d.value))
       })
   },
   
